e468af2408e57c3eb2920f6aab6f89dc1957a1f8
Ashish4684 <91828783+Ashish4684@users.noreply.github.com>
2024-01-29 07:55:24 +0000

    comparision an data type

--- 01_basics/04_comparisions.js
+++ 01_basics/04_comparisions.js
@@ -19,3 +19,24 @@ console.log(undefined < 0);
 // === 
 
 console.log("2" === 2);*/
+
+
+let a = 2 
+let b =  3
+
+console.log(a==b);
+console.log(a>b);
+console.log(a<b);
+
+console.log(a<=b);
+console.log(a>=b);
+
+let c = "2"
+console.log(a==c);
+console.log(a===c);
+
+let d = null 
+let e = 0
+console.log(e == d);
+console.log(e<d);
+console.log(e <= d);
---
+++ 01_basics/basicDataType.js
@@ -0,0 +1,90 @@
+// *****************DATA TYPES*********************************
+/*  //  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+*/
+
+// TWO TYPE OF DATATYPE CALLED *PRIMITIVE *  AND *NON PRIMITIVE* 
+
+// DIVIDE ON THE BASIS OF HOW DATA IS STORED AND HOW WE ACCESS THE DATA
+
+// 7 PRIMITIVE DATA  / CALL BY VALUE
+
+/*
+*STRING
+* NUMBER 
+*BOOLEAN 
+*null
+*UNDEFINED
+*BIGINT
+*SYMBOL
+**/
+
+
+// NON PRIMITIVE / CALL BY REFERENCE / 3 TYPE 
+/*
+*ARRAY
+* FUNCTIONS
+* OBJECTS
+*/
+
+const n = 389789
+const m = 667777
+console.log(typeof n );
+console.log(typeof m );
+const isLoggedIn = true;
+console.log(typeof isLoggedIn);
+const nm = undefined
+console.log(typeof nm);
+
+
+const my = [ "dgfu" , "dg i u" , "o uhgi8u"]
+console.log(my[2]);
+
+const myobject = {
+    1 : "ashish",
+    2 : "]dfjio"
+}
+console.log(myobject[1]);
+
+/*const myFunction = function names(para) {
+    console.log(para);
+    
+}
+let para = 2
+names(para)*/
+
+
+// *****************************NUMBER*****************************
