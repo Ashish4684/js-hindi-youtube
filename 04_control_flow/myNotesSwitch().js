95aedbc0efd31e06bdecfaf6cbe3a0b71abb0168
Ashish4684 <91828783+Ashish4684@users.noreply.github.com>
2024-02-03 13:19:40 +0000

    switch

---
+++ 04_control_flow/myNotesSwitch().js
@@ -0,0 +1,39 @@
+// ********any data type can be compared in switc case (just put the value in cases)***************
+
+let month = 2
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+
+    default: console.log("year end");
+        break;
+}
+// *****************************comparision of string*************************
+
+let mon = "jan"
+switch (mon) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+
+    default: console.log("year end");
+        break;
+}
+
+
+
+
+ 
